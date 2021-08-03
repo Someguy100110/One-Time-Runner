85c49fd42db1e6b9a061c61030f3a55f0ca7b3e0
Someguy100110 <somethingaboutgod90@gmail.com>
2021-08-03 10:54:42 -0400

    Fixed moving into different lanes, it should work smoothly now(while grounded)

--- Group project/Assets/Scripts/playerMovement.cs
+++ Group project/Assets/Scripts/playerMovement.cs
@@ -7,9 +7,13 @@ public class playerMovement : MonoBehaviour
 	public int speed = 10;
 	Rigidbody rb;
 	public int maxJumps = 2;
-	public float jumpForce = 8.0f;
-	int jumping = 2;
-	bool changingLanes = false;
+	public float jumpForce = 30.0f;
+	int jumping = 1;
+	int x = 0;
+	bool left = false;
+	bool right = false;
+	float timer = 0;
+	bool timerunning = false; 
 	// Start is called before the first frame update
 	void Start()
     {
@@ -20,15 +24,33 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A) && left == false && timerunning == false)
 		{
-			//transform.position.x = transform.position.x - 3;
+			x = x - 3;
+			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+			timer = 30.0f;
+			timerunning = true;
+		}
+		if (Input.GetKey(KeyCode.D) && right == false && timerunning == false)
+		{
+			x = x + 3;
+			transform.position = new Vector3(x, transform.position.y, transform.position.z);
+			timer = 30.0f;
+			timerunning = true;
 		}
 		transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        if(Input.GetButtonDown("Jump") && jumping > 0 && changingLanes == false)
+        if(Input.GetButtonDown("Jump") && jumping > 0)
 		{
 			jumping = jumping - 1;
 			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+		}
+        if(timer > 0)
+		{
+			timer -= 1;
+		}
+        else 
+		{
+			timerunning = false;
 		}
 	}
     void OnCollisionEnter(Collision other)
@@ -36,6 +58,20 @@ public class playerMovement : MonoBehaviour
         if(other.gameObject.CompareTag("Ground"))
 		{
 			jumping = maxJumps;
+			left = false;
+			right = false;
+		}
+		if (other.gameObject.CompareTag("LeftLane"))
+		{
+			left = true;
+			right = false;
+			jumping = maxJumps;
+		}
+		if (other.gameObject.CompareTag("RightLane"))
+		{
+			jumping = maxJumps;
+			left = false;
+			right = true;
 		}
 	}
 }
